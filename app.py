d3e766d043800011ff3ea9097259552fb3828037
Justis Corbett <corbettjustis@gmail.com>
2021-10-23 02:34:12 -0600

    fix sqlalchemy postgres uri

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 DATABASE_URL = os.environ['DATABASE_URL']
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/flasktrails'
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
